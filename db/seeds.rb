af3fe80b7a297dc89f1d9def82ac498510f9140a
Jelena <jelena.durovic@powerhrg.com>
2021-09-24 14:10:06 -0400

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+papasidos = Restaurant.create(name: "Papasidos", description: "Spanish restaurant for family", city: "NY", star_rating: 3, cuisine: "Spanish")
+mastros = Restaurant.create(name: "Mastros", description: "Very fancy place", city: "Houston", star_rating: 5, cuisine: "American")
+steak48 = Restaurant.create(name: "Steak48", description: "Amazing food", city: "Philly", star_rating: 4, cuisine: "American")
+ruths = Restaurant.create(name: "Ruth's Chris Steakhouse", description: "all over the country", city: "New Orleans", star_rating: 5, cuisine: "Mediterenian")
